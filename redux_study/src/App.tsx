4a40514e2ab7c9033013d43d458db47152c95a87
soo777 <ssm19944@gmail.com>
2020-05-05 21:04:11 +0900

    change to using hooks

--- redux_study/src/App.tsx
+++ redux_study/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import CounterContainer from "./containers/CounterContainer";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <CounterContainer/>
-  );
+  return <Counter />;
 }
 
 export default App;
