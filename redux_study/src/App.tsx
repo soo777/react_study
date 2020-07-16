cf3e8c3d7cfbb45393dc6665342d8aed8e391d3b
soo777 <ssm19944@gmail.com>
2020-07-16 21:50:15 +0900

    hooks - useContext, useMemo

--- redux_study/src/App.tsx
+++ redux_study/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import Info from "./components/Info";
+import ContextSample from "./components/ContextSample";
+import Average from "./components/Average";
 
 function App() {
   return (
       <>
         <Counter/>
         <Info/>
+        <ContextSample/>
+        <Average/>
       </>
   )
 }
---
+++ redux_study/src/components/Average.tsx
@@ -0,0 +1,43 @@
+import React, { useMemo, useState } from "react";
+
+const getAverage = (numbers:any) => {
+  console.log("평균값 계산중");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a:any, b:any) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+
+  const onChange = (e:any) => {
+    setNumber(e.target.value);
+  };
+
+  const onInsert = (e:any) => {
+    // @ts-ignore
+    const nextList = list.concat(parseInt(number, 10));
+    setList(nextList);
+    setNumber("");
+  };
+
+  const avg = useMemo(() => getAverage(list), [list]);
+
+  return (
+    <div>
+      <input value={number} onChange={onChange}/>
+      <button onClick={onInsert}>등록</button>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <b>평균 값 : </b> {avg}
+      </div>
+    </div>
+  );
+};
+
+export default Average;
