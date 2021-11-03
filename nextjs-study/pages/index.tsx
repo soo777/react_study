59ad840d2674329c1ec12f658b40a06019386037
soo777 <ssm19944@gmail.com>
2021-11-03 21:32:21 +0900

    nextjs - 공유컴포넌트

--- nextjs-study/pages/index.tsx
+++ nextjs-study/pages/index.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Header from "./components/Header";
+import React from "react";
+import Layout from "./components/Layout";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <div>
-        <Link href="/about">
-          {/*<a style={{fontSize: '20px'}}>About page</a>*/}
-          <button>Got to About page</button>
-        </Link>
-      </div>
+    // <div>
+    //   <div>
+    //     <Header />
+    //     <Link href="/about">
+    //       <button>Got to About page</button>
+    //     </Link>
+    //   </div>
+    //
+    // </div>
 
-    </div>
+    <Layout>
+      <p>This is the about apge</p>
+    </Layout>
   )
 }
 
