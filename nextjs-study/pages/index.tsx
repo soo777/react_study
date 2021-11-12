93637ad45980440dea6cd7c3b150525267c53c00
soo777 <ssm19944@gmail.com>
2021-11-12 21:11:51 +0900

    간단하게 json-server 세팅

--- nextjs-study/pages/index.tsx
+++ nextjs-study/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./components/Layout";
+import axios from "axios";
 
 const PostLink = (props:any) => (
   <li>
@@ -12,15 +13,39 @@ const PostLink = (props:any) => (
   </li>
 )
 
-const Home: NextPage = () => (
+const Home: NextPage = () => {
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const result =  await axios.get('http://localhost:4000/posts');
+      console.log(result.data);
+
+      setArr(result.data);
+    }
+
+    // json-server 세팅 필요
+    getData();
+  }, [])
+
+
+  return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js"/>
-        <PostLink title="Learn Next.js is awesome"/>
-        <PostLink title="Deploy apps with Zeit"/>
+        {/*<PostLink title="Hello Next.js"/>*/}
+        {/*<PostLink title="Learn Next.js is awesome"/>*/}
+        {/*<PostLink title="Deploy apps with Zeit"/>*/}
+        {
+          arr.map((data:any)=> (
+            <PostLink key={data.id} title={data.title}/>
+          ))
+        }
       </ul>
     </Layout>
-)
+  )
+
+}
+
 
 export default Home
